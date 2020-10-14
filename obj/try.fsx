3010ede0e5f663e3dcb3f1932d3905cc25c904f5
Vriddhi Pai <49198210+vriddhipai@users.noreply.github.com>
2020-10-14 15:02:34 -0400

    Add files via upload

--- obj/try.fsx
+++ obj/try.fsx
@@ -13,6 +13,8 @@ type ProjTwo =
     | ReportMsgRecvd of String
     | TimeCount of int
     | NodeCount of int
+    | Push of double * double
+    | PushSumAnswer of double * double
 
 let rnd = System.Random(1)
 
@@ -38,16 +40,24 @@ type ActorCount() =
                 printfn "Time for convergence: %i ms" (endTime - startTime)
                 Environment.Exit(0)
 
+        | PushSumAnswer (s, w) ->
+            printfn "sum %f weight %f average %f" s w (s / w)
+            printfn "Time of convergence endtime - startime"
+            Environment.Exit 0
+
         | TimeCount strtTime -> startTime <- strtTime
 
         | NodeCount numofnodes -> numOfNodes <- numofnodes
 
         | _ -> ()
 
-type Node(actorCount: IActorRef, neighborlist: int [], nodestopping: int) =
+type Node(actorCount: IActorRef, totalnodes: int, neighborlist: IActorRef) =
     inherit Actor()
     let mutable numMsgHeard = 0
     let mutable nghbrs: IActorRef [] = [||]
+    let mutable rounds = 0
+    let mutable s = totalnodes |> double
+    let mutable w = 1.0
 
     override x.OnReceive(num) =
 
@@ -64,8 +74,31 @@ type Node(actorCount: IActorRef, neighborlist: int [], nodestopping: int) =
                 let index = System.Random().Next(0, nghbrs.Length)
                 nghbrs.[index] <! Algorithm(msg)
 
-        | _ -> ()
+        | Push (incomingS, incomingW) ->
+            let mutable newS = s + incomingS
+            let mutable newW = w + incomingW
 
+            let difference = abs ((newS / newW) - (s / w))
+
+            if (difference > pown 10.0 -10) then
+                rounds <- 0
+                s <- s / 2.0
+                w <- w / 2.0
+
+                let index = System.Random().Next(0, nghbrs.Length)
+                nghbrs.[index] <! Push(s, w)
+
+            elif (rounds >= 3) then
+                actorCount <! PushSumAnswer(s, w)
+
+            else
+                let index = System.Random().Next(0, nghbrs.Length)
+                s <- s / 2.0
+                w <- w / 2.0
+                nghbrs.[index] <! Push(s, w)
+                rounds <- rounds + 1
+
+        | _ -> ()
 
 //get input args
 let mutable numNodes =
@@ -84,15 +117,16 @@ match topology with
     let buildTopology = Array.zeroCreate (numNodes + 1) //to store all actor nodes in one place
 
     for i in [ 0 .. numNodes + 1 ] do
-        buildTopology.[i] <- (spawn system (string i) Node) //The actor function needs to be passed here; fix this
+        buildTopology.[i] <- system.ActorOf(Props.Create(typeof<Node>, actorCount, numNodes, neighborlist), "ProjTwo") //The actor function needs to be passed here; fix this
 
     for i in [ 0 .. numNodes + 1 ] do //list that stores the neighbors for the node i; here all actors are added to the list for every i
         neighborlist <- buildTopology.[i] :: neighborlist //:: operator is used to append to the current list; mutable
 
     //to do: call the node function with the list and limit 10
-    //buildtopology called on the index of current node, its list of neighbors, total nodes to ping
 
-    let boss = System.Random().Next(0, numNodes)
+    let listlength = neighborlist.Length //since a random neighbor is selected to send rumor
+
+    let boss = System.Random().Next(0, listlength)
     if protocol = "gossip" then
         actorCount <! NodeCount(numNodes)
         actorCount
@@ -101,12 +135,28 @@ match topology with
         buildTopology.[boss]
         <! Algorithm("Gossip Protocol")
 
+    if protocol = "pushsum" then
+        for i in [ 0 .. numNodes + 1 ] do //picking a random neighbor to send s/w pair
+            let mutable s = i
+            let mutable w = 1.0
+
+            //for current i, set s and w, find a random neighbor and send your s/w pair information
+            for i in [ 0 .. listlength + 1 ] do //picking the random neighbor from the list
+                actorCount (numNodes)
+                actorCount
+                <! TimeCount(System.DateTime.Now.TimeOfDay.Milliseconds) //time started
+                printfn "Result from Protocol Pushsum"
+                buildTopology.[boss]
+                <! PushSumAnswer(s / 2, w / 2)
+//in the node function,
+
+
 | "line" -> //actors placed linearly; actor at position i has neighbors at i+1 and i-1; max neighbrs possible 2, min 1
     let mutable neighborlist: IActorRef list = [] //list that stores neighbors of current node i
     let buildTopology = Array.zeroCreate (numNodes + 1) //to store all actor nodes in one place
 
     for i in [ 0 .. numNodes + 1 ] do
-        buildTopology.[i] <- (spawn system (string i) Node) //the actor function should be passed here; please check this
+        buildTopology.[i] <- system.ActorOf(Props.Create(typeof<Node>, actorCount, numNodes, neighborlist), "ProjTwo") //the actor function should be passed here; please check this
 
     for i in [ 0 .. numNodes + 1 ] do
         if i = 1 then
@@ -189,12 +239,12 @@ match topology with
             buildTopology.[boss]
             <! Algorithm("Gossip Protocol")
 
-
 | "Imp2D" ->
     let mutable neighborlist: IActorRef list = [] //list that stores neighbors of current node i
     let buildTopology = Array.zeroCreate (numNodes + 1) //to store all actor nodes in one place
     for i in [ 0 .. numNodes + 1 ] do
-        buildTopology.[i] <- (spawn system (string i) Node)
+        buildTopology.[i] <- system.ActorOf
+                                 (Props.Create(typeof<Node>, actorCount, numNodes, neighborlist), "ProjTwo" + string (i))
     let mutable squaredNode = getPerfectSquareNum (float numNodes)
     let mutable nodessqrt = sqrt (float numNodes)
     let mutable intsqrt = int (nodessqrt) //int conversion of nodessqrt
