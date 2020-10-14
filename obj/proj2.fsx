0f1905a14e5505cbabd9a3ca5bb05554ce63c6a4
venkata sindhu kandula <venkata776@yahoo.com>
2020-10-14 00:10:59 -0400

    Added line, 2D for gossip - Working

--- obj/proj2.fsx
+++ obj/proj2.fsx
@@ -66,9 +66,16 @@ type Node(actorCount: IActorRef, numResend: int, nodeNum: int)=
 //get input args
 let mutable numNodes = int(string (fsi.CommandLineArgs.GetValue 1))
 let topology = string (fsi.CommandLineArgs.GetValue 2)
-let protocol= string (fsi.CommandLineArgs.GetValue 3)
+let algorithm = string (fsi.CommandLineArgs.GetValue 3)
 
 let system = ActorSystem.Create("System")
+let mutable actualNumOfNodes=float(numNodes)
+
+
+numNodes = if topology="2D" || topology="imp2D" then 
+                 int(floor((actualNumOfNodes ** 0.5) ** 2.0))
+             else
+                 numNodes
          
 let actorCount=system.ActorOf(Props.Create(typeof<ActorCount>),"actorCount")
 
@@ -81,11 +88,55 @@ match topology  with
               buildTopology.[i]<!Initailize(buildTopology)
               
           let boss = System.Random().Next(0,numNodes)
-          if protocol="gossip" then
+          if algorithm="gossip" then
             actorCount<!NodeCount(numNodes)
             actorCount<!TimeCount(System.DateTime.Now.TimeOfDay.Milliseconds)
             printfn "Result for Protocol Gossip"
-            buildTopology.[boss]<!Algorithm("Gossip Protocol")
+            buildTopology.[boss]<!Algorithm("Gossip Algorithm for Full Topology")
+
+      |"line"->
+          let buildTopology= Array.zeroCreate (numNodes+1)
+          for i in [0..numNodes] do
+              buildTopology.[i]<-system.ActorOf(Props.Create(typeof<Node>,actorCount,10,i+1),"ProjTwo"+string(i))
+          for i in [0..numNodes] do
+              let nghbrArray=[|buildTopology.[((i-1+numNodes)%(numNodes+1))];buildTopology.[((i+1+numNodes)%(numNodes+1))]|]
+              buildTopology.[i]<!Initailize(nghbrArray)
+          let boss = System.Random().Next(0,numNodes)
+          if algorithm="gossip" then
+            actorCount<!NodeCount(numNodes)
+            actorCount<!TimeCount(System.DateTime.Now.TimeOfDay.Milliseconds)
+            printfn "Result for Protocol Gossip"
+            buildTopology.[boss]<!Algorithm("Gossip Algorithm for Line Topology")
+
+
+      |"2D"->
+           let gridSize=int(ceil(sqrt actualNumOfNodes))
+           let gridCount=gridSize*gridSize
+           let buildTopology= Array.zeroCreate (gridCount)
+           for i in [0..(gridSize*gridSize-1)] do
+              buildTopology.[i]<-system.ActorOf(Props.Create(typeof<Node>,actorCount,10,i+1),"ProjTwo"+string(i))
+           
+           for i in [0..gridSize-1] do
+               for j in [0..gridSize-1] do
+                    let mutable nghbrs:IActorRef[]=[||]
+                    if j+1<gridSize then
+                        nghbrs<-(Array.append nghbrs [|buildTopology.[i*gridSize+j+1]|])
+                    if j-1>=0 then
+                        nghbrs<-Array.append nghbrs [|buildTopology.[i*gridSize+j-1]|]
+                    if i-1>=0 then
+                        nghbrs<-Array.append nghbrs [|buildTopology.[(i-1)*gridSize+j]|]
+                    if i+1<gridSize then
+                        nghbrs<-(Array.append nghbrs [|buildTopology.[(i+1)*gridSize+j]|])
+                    buildTopology.[i*gridSize+j]<!Initailize(nghbrs)
+
+       
+               
+           let boss = System.Random().Next(0,gridCount-1)  
+           if algorithm="gossip" then
+            actorCount<!NodeCount(gridCount-1)
+            actorCount<!TimeCount(System.DateTime.Now.TimeOfDay.Milliseconds)
+            printfn "Result for Protocol Gossip"
+            buildTopology.[boss]<!Algorithm("Gossip Algorithm for 2D Topology")
 
 
       | _-> ()
